dce18dd1a5a3c6e5f596ff93a129a7aabd99c2ad
letigiam <64791044+letigiam@users.noreply.github.com>
2020-11-30 17:00:57 +0100

    Update caseReduce.js

--- caseReduce.js
+++ caseReduce.js
@@ -1,5 +1,4 @@
-// var module = require('letigiamnpmtest');
-// console.log(module.players); 
+//import file index.js
 import players from "./index.js";
 
 var users = (userId) => players.find(item => item.id == userId);
@@ -13,17 +12,9 @@ var userNameOrSurname = (name, surname) => {
 console.log(userNameOrSurname('Luigi', ''));
 
 
-
-// var userSurname = (surname) => players.filter(item => item.surname == surname);
-// console.log(userSurname('Allan'));
-
 var userNational = (nationality) => players.every(item => item.nationality == nationality);
 console.log(userNational('Italy'));
 
 var userSick = (nationality) =>  players.some(item => item.sick == nationality);
 console.log(userSick('yes'));
 
-// console.log('userName: ', userName);
-// console.log('userSurname: ', userSurname);
-// console.log('userNational: ', userNational);
-// console.log('userSick: ', userSick);
